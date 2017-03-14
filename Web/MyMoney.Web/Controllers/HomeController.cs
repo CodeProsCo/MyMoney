414776fc7da5d1744cb3ad99f9edba7b473a333a
David Bond <david@david-bond.net>
2017-03-14 22:44:21 +0000

    Modified Files
    Added documentation.

--- Web/MyMoney.Web/Controllers/HomeController.cs
+++ Web/MyMoney.Web/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace MyMoney.Web.Controllers
+﻿namespace MyMoney.Web.Controllers
 {
+    #region Usings
+
+    using System.Web.Mvc;
+
+    #endregion
+
+    /// <summary>
+    /// The <see cref="HomeController"/> class handles the default route.
+    /// </summary>
+    /// <seealso cref="MyMoney.Web.Controllers.BaseController" />
     [AllowAnonymous]
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        #region  Public Methods
+
+        /// <summary>
+        /// The main endpoint for the application. Redirects based on authentication.
+        /// </summary>
+        /// <returns>If authenticated, the budget dashboard. Otherwise, returns to the login screen.</returns>
         [AllowAnonymous]
         public ActionResult Index()
         {
@@ -16,5 +26,7 @@ namespace MyMoney.Web.Controllers
                        ? RedirectToAction("BudgetOverview", "Budget", new { area = "Dashboard" })
                        : RedirectToAction("Login", "User", new { area = "Authentication" });
         }
+
+        #endregion
     }
 }
