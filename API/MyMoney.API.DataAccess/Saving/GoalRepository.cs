fab725573d0bb88fc8e75ecab05f20386d0e4666
David Bond <david@david-bond.net>
2017-04-03 19:47:44 +0100

    Modified Files
    Code cleanup and documentation.

--- API/MyMoney.API.DataAccess/Saving/GoalRepository.cs
+++ API/MyMoney.API.DataAccess/Saving/GoalRepository.cs
@@ -16,17 +16,31 @@
 
     #endregion
 
+    /// <summary>
+    /// The <see cref="GoalRepository"/> class performs CRUD actions on the database for instances of the <see cref="GoalDataModel"/> class.
+    /// </summary>
+    /// <seealso cref="MyMoney.API.DataAccess.Saving.Interfaces.IGoalRepository" />
     [UsedImplicitly]
     public class GoalRepository : IGoalRepository
     {
         #region Fields
 
+        /// <summary>
+        /// The context
+        /// </summary>
         private readonly IDatabaseContext context;
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GoalRepository"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <exception cref="System.ArgumentNullException">context
+        /// Exception thrown if the given database context is null.
+        /// </exception>
         public GoalRepository(IDatabaseContext context)
         {
             if (context == null)
@@ -41,6 +55,13 @@
 
         #region Methods
 
+        /// <summary>
+        /// Adds a goal to the database.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns>
+        /// The newly added goal.
+        /// </returns>
         public async Task<GoalDataModel> AddGoal(GoalDataModel model)
         {
             model.Id = Guid.NewGuid();
@@ -53,6 +74,16 @@
             return rows > 0 ? model : null;
         }
 
+        /// <summary>
+        /// Deletes a goal from the database.
+        /// </summary>
+        /// <param name="goalId">The goal identifier.</param>
+        /// <returns>
+        /// If true, deletion was successful. False if not.
+        /// </returns>
+        /// <exception cref="System.Exception">
+        /// Exception thrown if the requested goal cannot be found.
+        /// </exception>
         public async Task<bool> DeleteGoal(Guid goalId)
         {
             var toDelete = await GetGoal(goalId);
@@ -70,6 +101,16 @@
             return rows > 0;
         }
 
+        /// <summary>
+        /// Updates a goal in the database.
+        /// </summary>
+        /// <param name="model">The updated model.</param>
+        /// <returns>
+        /// The updated data model.
+        /// </returns>
+        /// <exception cref="System.Exception">
+        /// Exception thrown if the requested goal cannot be found.
+        /// </exception>
         public async Task<GoalDataModel> EditGoal(GoalDataModel model)
         {
             var toEdit = await GetGoal(model.Id);
@@ -90,6 +131,16 @@
             return rows > 0 ? model : null;
         }
 
+        /// <summary>
+        /// Obtains a goal from the database with the given identifier.
+        /// </summary>
+        /// <param name="goalId">The goal identifier.</param>
+        /// <returns>
+        /// The goal.
+        /// </returns>
+        /// <exception cref="System.Exception">
+        /// Exception thrown if the requested goal cannot be found.
+        /// </exception>
         public async Task<GoalDataModel> GetGoal(Guid goalId)
         {
             var goal = await context.Goals.FirstOrDefaultAsync(x => x.Id.Equals(goalId));
@@ -102,6 +153,13 @@
             return goal;
         }
 
+        /// <summary>
+        /// Gets all goals for a given user.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns>
+        /// The list of goals.
+        /// </returns>
         public async Task<IList<GoalDataModel>> GetGoalsForUser(Guid userId)
         {
             return await context.Goals.Where(x => x.UserId.Equals(userId)).ToListAsync();
--- API/MyMoney.API.DataAccess/Saving/Interfaces/IGoalRepository.cs
+++ API/MyMoney.API.DataAccess/Saving/Interfaces/IGoalRepository.cs
@@ -10,18 +10,46 @@
 
     #endregion
 
+    /// <summary>
+    /// Interface for the <see cref="GoalRepository"/> class.
+    /// </summary>
     public interface IGoalRepository
     {
         #region Methods
 
+        /// <summary>
+        /// Deletes a goal from the database.
+        /// </summary>
+        /// <param name="goalId">The goal identifier.</param>
+        /// <returns>If true, deletion was successful. False if not.</returns>
         Task<bool> DeleteGoal(Guid goalId);
 
+        /// <summary>
+        /// Updates a goal in the database.
+        /// </summary>
+        /// <param name="model">The updated model.</param>
+        /// <returns>The updated data model.</returns>
         Task<GoalDataModel> EditGoal(GoalDataModel model);
 
+        /// <summary>
+        /// Obtains a goal from the database with the given identifier.
+        /// </summary>
+        /// <param name="goalId">The goal identifier.</param>
+        /// <returns>The goal.</returns>
         Task<GoalDataModel> GetGoal(Guid goalId);
 
+        /// <summary>
+        /// Gets all goals for a given user.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns>The list of goals.</returns>
         Task<IList<GoalDataModel>> GetGoalsForUser(Guid userId);
 
+        /// <summary>
+        /// Adds a goal to the database.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns>The newly added goal.</returns>
         Task<GoalDataModel> AddGoal(GoalDataModel model);
 
         #endregion
