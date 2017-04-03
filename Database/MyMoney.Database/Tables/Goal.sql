660cfddcac2c8b9e0fd70f5d6cbe51a57e6b4a43
David Bond <david@david-bond.net>
2017-04-03 19:19:36 +0100

    Modified Files
    Created Goal table.

---
+++ Database/MyMoney.Database/Tables/Goal.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Goal]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newsequentialid(), 
+    [Name] NVARCHAR(32) NOT NULL, 
+    [StartDate] DATE NOT NULL, 
+    [EndDate] DATE NULL, 
+    [UserId] UNIQUEIDENTIFIER NOT NULL, 
+    [Amount] FLOAT NOT NULL, 
+    [Complete] BIT NOT NULL DEFAULT 0, 
+
+    CONSTRAINT [FK_Goal_ToUser] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
