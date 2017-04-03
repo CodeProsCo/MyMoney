167921ccc4ffecf92a31c6dcf0e3a4240baee056
David Bond <david@david-bond.net>
2017-04-03 19:53:16 +0100

    Modified Files
    Added documentation to datamodel. Removed database project from debug
    build.

--- Models/MyMoney.DataModels/Saving/GoalDataModel.cs
+++ Models/MyMoney.DataModels/Saving/GoalDataModel.cs
@@ -1,31 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyMoney.DataModels.Saving
+﻿namespace MyMoney.DataModels.Saving
 {
+    #region Usings
+
+    using System;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
+    #endregion
+
+    /// <summary>
+    /// The <see cref="GoalDataModel"/> represents an entry in the "Goal" table.
+    /// </summary>
+    /// <seealso cref="MyMoney.DataModels.BaseDataModel" />
     [Table("Goal")]
     public class GoalDataModel : BaseDataModel
     {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the amount.
+        /// </summary>
+        /// <value>
+        /// The amount.
+        /// </value>
+        public double Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="GoalDataModel"/> is complete.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if complete; otherwise, <c>false</c>.
+        /// </value>
+        public bool Complete { get; set; }
+
+        /// <summary>
+        /// Gets or sets the end date.
+        /// </summary>
+        /// <value>
+        /// The end date.
+        /// </value>
+        public DateTime? EndDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
-        public Guid UserId { get; set; }
-
-        public double Amount { get; set; }
-
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
 
-        public bool Complete { get; set; }
-
+        /// <summary>
+        /// Gets or sets the start date.
+        /// </summary>
+        /// <value>
+        /// The start date.
+        /// </value>
         public DateTime StartDate { get; set; }
 
-        public DateTime? EndDate { get; set; }
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+        public Guid UserId { get; set; }
+
+        #endregion
     }
 }
