4cf1b20aa225209b4553c3f9aa68018820efaf44
David Bond <david@david-bond.net>
2017-04-29 22:56:44 +0100

    Added Tests
    100% coverage for BillAssembler with a bit of refactoring.

---
+++ API/MyMoney.API.Assemblers.Tests/Spending/ExpenditureAssemblerTests.cs
@@ -0,0 +1,30 @@
+﻿namespace MyMoney.API.Assemblers.Tests.Spending
+{
+    #region Usings
+
+    using Assemblers.Spending;
+    using Assemblers.Spending.Interfaces;
+
+    using NUnit.Framework;
+
+    #endregion
+
+    [TestFixture]
+    [Category("API Assemblers")]
+    public class ExpenditureAssemblerTests
+    {
+        private IExpenditureAssembler assembler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            assembler = new ExpenditureAssembler();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            assembler = null;
+        }
+    }
+}
