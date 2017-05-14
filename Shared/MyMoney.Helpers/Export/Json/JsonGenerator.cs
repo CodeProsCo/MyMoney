76371fac5fce9f5487b3da6303ad084577173c68
David Bond <david@david-bond.net>
2017-05-14 20:42:19 +0100

    Modified files
    Tweaked the JsonGenerator, can confirm it works as expected.

--- Shared/MyMoney.Helpers/Export/Json/JsonGenerator.cs
+++ Shared/MyMoney.Helpers/Export/Json/JsonGenerator.cs
@@ -2,24 +2,51 @@
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// The <see cref="JsonGenerator"/> class converts objects into a JSON file format.
+    /// </summary>
     public class JsonGenerator
     {
+        /// <summary>
+        /// The node format
+        /// </summary>
         private const string NodeFormat = "{{\n{0}\n}},\n";
 
-        private const string PropertyFormat = "\t{0}: {1},\n";
+        /// <summary>
+        /// The property format
+        /// </summary>
+        private const string PropertyFormat = "\t{0}: \"{1}\",\n";
 
-        private const string BodyFormat = "{{[{0}]}}";
+        /// <summary>
+        /// The body format
+        /// </summary>
+        private const string BodyFormat = "{{data: [{0}]}}";
 
+        /// <summary>
+        /// The nodes
+        /// </summary>
         private readonly IList<object> nodes;
 
+        /// <summary>
+        /// The node properties
+        /// </summary>
         private readonly IList<string> nodeProperties;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonGenerator"/> class.
+        /// </summary>
         public JsonGenerator()
         {
             nodes = new List<object>();
             nodeProperties = new List<string>();
         }
 
+        /// <summary>
+        /// Adds the nodes.
+        /// </summary>
+        /// <typeparam name="T">The type of object.</typeparam>
+        /// <param name="data">The data.</param>
+        /// <returns>This instance, for function chaining.</returns>
         public JsonGenerator AddNodes<T>(IEnumerable<T> data)
         {
             foreach (var node in data)
@@ -30,7 +57,12 @@
             return this;
         }
 
-        public JsonGenerator AddNodeProperties(IList<string> properties)
+        /// <summary>
+        /// Adds the node properties.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        /// <returns>This instance, for function chaining.</returns>
+        public JsonGenerator AddNodeProperties(IEnumerable<string> properties)
         {
             foreach (var prop in properties)
             {
@@ -40,6 +72,12 @@
             return this;
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             var nodesString = string.Empty;
